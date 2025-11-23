7ba194be7a5d047113bc967c90ab37f67ca5b881
Akshay Raj Gollahalli <akshay@gollahalli.com>
2025-11-23 21:49:43 +1300

    Trim file name

--- tests/utils_tests.rs
+++ tests/utils_tests.rs
@@ -79,3 +79,11 @@ async fn test_get_size_non_existent_path() {
     assert_eq!(count, 0);
     assert_eq!(size, 0);
 }
+
+#[test]
+fn test_trim_filename() {
+    assert_eq!(cp2::utils::trim_filename("short.txt", 20), "short.txt");
+    assert_eq!(cp2::utils::trim_filename("this_is_a_very_long_filename.txt", 20), "this_is_a...name.txt");
+    assert_eq!(cp2::utils::trim_filename("medium_length_name.txt", 10), "medi...txt");
+    assert_eq!(cp2::utils::trim_filename("tiny", 3), "...");
+}
